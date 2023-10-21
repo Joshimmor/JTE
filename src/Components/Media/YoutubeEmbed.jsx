17408c59f418fa642edd00a05d12d1d0c0bcdce2
Joshimmor <Joshimmor@gmail.com>
2023-10-21 19:38:39 -0400

    video player

--- src/Components/Media/YoutubeEmbed.jsx
+++ src/Components/Media/YoutubeEmbed.jsx
@@ -1,22 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
+import ReactPlayer from "react-player";
 
-const YoutubeEmbed = ({ embedId }) => (
-  <div className="video-responsive">
-    <iframe
-      width="853"
-      height="480"
-      src={`https://www.youtube.com/embed/${embedId}`}
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-      title="Embedded youtube"
-    />
-  </div>
-);
 
-YoutubeEmbed.propTypes = {
-  embedId: PropTypes.string.isRequired
-};
-
-export default YoutubeEmbed;
+export default function YoutubeEmbed({embedId}){
+  return <ReactPlayer
+      url={`https://www.youtube.com/embed/${embedId}`}
+      controls
+      playing={false}
+      />
+}
