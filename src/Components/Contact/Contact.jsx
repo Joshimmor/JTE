1b56b242800496d261c33f97f9566b69569a266c
Joshimmor <Joshimmor@gmail.com>
2023-10-25 09:02:02 -0400

    fixing a transition i grew to hate

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import "./contact.css"
+import "./Contact.css"
 import { useSpring,animated} from '@react-spring/web'
 
 export default function Contact({contactRef}) {
@@ -15,7 +15,7 @@ export default function Contact({contactRef}) {
         <div className="textbox">
             <h2  onClick={()=> setVisable(!visable)}className='contacttext'>CONTACT</h2>
             <animated.h4 className='contactsubtext'style={{ opacity: opacity.to(o => 1 - o), transform }}>WHO?</animated.h4> 
-            <animated.h6 className='contactsubsubtext' style={{ opacity: opacity.to(o => 1 - o), transform,animationDelay:'10ms' }}>PROJECTS</animated.h6>
+            <animated.h6 className='contactsubsubtext' style={{ opacity: opacity.to(o => 1 - o), transform }}>PROJECTS</animated.h6>
             <animated.h6 className='contactsubsubtext' style={{ opacity: opacity.to(o => 1 - o), transform }}>PRESS</animated.h6>
             <animated.h6 className='contactsubsubtext' style={{ opacity: opacity.to(o => 1 - o), transform }}>DESIGN</animated.h6>
         </div>
