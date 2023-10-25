108c24d921d0ceb22c05c0284e935da2602ad045
Joshimmor <Joshimmor@gmail.com>
2023-10-25 11:46:24 -0400

    contact animation

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -4,20 +4,21 @@ import { useSpring,animated} from '@react-spring/web'
 
 export default function Contact({contactRef}) {
   let [visable,setVisable] = useState(true)
-  const { transform, opacity } = useSpring({
+  const { transform, opacity,display } = useSpring({
     opacity: visable ? 1 : 0,
-    transform: `perspective(600px) rotateX(${visable ? -90 : 0}deg)`,
-    config: { mass: 5, tension: 500, friction: 80 },
+    display: visable ? "none":"block",
+    transform: `perspective(600px) translateX(${visable ? 90 : 0}em)`,
+    config: { mass: 4, tension: 400, friction: 90 },
   })
   
   return (
     <div className='contact' ref={contactRef}>
         <div className="textbox">
             <h2  onClick={()=> setVisable(!visable)}className='contacttext'>CONTACT</h2>
-            <animated.h4 className='contactsubtext'style={{ opacity: opacity.to(o => 1 - o), transform }}>WHO?</animated.h4> 
-            <animated.h6 className='contactsubsubtext' style={{ opacity: opacity.to(o => 1 - o), transform }}>PROJECTS</animated.h6>
-            <animated.h6 className='contactsubsubtext' style={{ opacity: opacity.to(o => 1 - o), transform }}>PRESS</animated.h6>
-            <animated.h6 className='contactsubsubtext' style={{ opacity: opacity.to(o => 1 - o), transform }}>DESIGN</animated.h6>
+            <animated.h4  className='contactsubtext'style={{ opacity: opacity.to(o => 1 - o), transform,display }}>WHO?</animated.h4> 
+            <animated.a href="mailto:Joshimmor@gmail.com"  className='contactsubsubtext' style={{ opacity: opacity.to(o => 1 - o), transform,display  }}>PROJECTS</animated.a>
+            <animated.a href="mailto:Joshimmor@gmail.com" className='contactsubsubtext' style={{ opacity: opacity.to(o => 1 - o), transform,display  }}>PRESS</animated.a>
+            <animated.a  href="mailto:Joshimmor@gmail.com" className='contactsubsubtext' style={{ opacity: opacity.to(o => 1 - o), transform,display  }}>DESIGN</animated.a>
         </div>
     </div>
   )
